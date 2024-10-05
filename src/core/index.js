4cf4c02d8e6fa0e6596ec0fbdaad4f8d0fd1e8b6
lencedev <owen1.bolling@epitech.eu>
2024-10-05 13:53:49 +0200

    addjob

--- src/core/index.js
+++ src/core/index.js
@@ -3,3 +3,11 @@
 export function render(component, container) {
     container.innerHTML = component();
 }
+// Créer une fonction `addJob` pour ajouter un job à la liste
+export function addJob(newJob, jobs, renderFunction, container) {
+    // Ajouter le nouveau job à la liste existante
+    jobs.push(newJob);
+
+    // Re-render la liste mise à jour
+    render(renderFunction, container);
+}
