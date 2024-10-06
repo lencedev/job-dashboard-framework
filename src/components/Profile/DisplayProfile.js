e940ffb4e308b139ab463ea4692103cb5b48067d
lencedev <owen1.bolling@epitech.eu>
2024-10-06 13:35:17 +0200

    update

--- src/components/Profile/DisplayProfile.js
+++ src/components/Profile/DisplayProfile.js
@@ -1,11 +1,27 @@
-import { getUserInfo } from "./Profile.js";
+// src/components/profile/DisplayProfile.js
+
+import { getUserInfo } from './Profile.js';
 
 export function displayProfile() {
     const user = getUserInfo();
-    document.getElementById('app').innerHTML = `
-    <div class="profile-container">
-    <h2> Bienvenue, ${user.name}</h2>
-    <p>Email: ${user.email}</p>
-    </div>
+    const appElement = document.getElementById('app');
+
+    // Sauvegarder le contenu initial de `appElement`
+    const initialContent = appElement.innerHTML;
+
+    // Afficher la page de profil
+    appElement.innerHTML = `
+        <div class="profile-container">
+            <h2> Bienvenue, ${user.name}</h2>
+            <p>Email: ${user.email}</p>
+            <button id="close-profile-button">Fermer</button>
+        </div>
     `;
+
+    // Ajouter un événement pour fermer le profil
+    const closeButton = document.getElementById('close-profile-button');
+    closeButton.addEventListener("click", () => {
+        // Restaurer le contenu initial
+        appElement.innerHTML = initialContent;
+    });
 }
