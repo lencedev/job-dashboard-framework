fe11e13a9626686739e6b74d0e6943ef8881c165
lencedev <owen1.bolling@epitech.eu>
2024-10-07 10:11:31 +0200

    update

--- src/components/JobList/JobComponent.js
+++ src/components/JobList/JobComponent.js
@@ -1,14 +1,14 @@
 // src/components/JobComponent.js
 
 export function JobComponent(jobData) {
-    const jobId = `job-${jobData.id}`; // Génère un id unique pour ce job
+  const jobId = `job-${jobData.id}`; // Génère un id unique pour ce job
 
-    // La fonction qui sera appelée lorsqu'on clique sur le job
-    function handleClick() {
-        alert(`More details about: ${jobData.title}`);
-    }
-    // Retournons le HTML
-    return `
+  // La fonction qui sera appelée lorsqu'on clique sur le job
+  function handleClick() {
+    alert(`More details about: ${jobData.title}`);
+  }
+  // Retournons le HTML
+  return `
       <div id="${jobId}" class="job-item" style="cursor: pointer;">
         <h2>${jobData.title}</h2>
         <p>${jobData.shortDescription}</p>
@@ -18,8 +18,8 @@ export function JobComponent(jobData) {
 
 // Ajoute une fonction pour attacher le gestionnaire de clic après le rendu
 export function attachJobClickHandler(jobData) {
-    const jobId = `job-${jobData.id}`;
-    document.getElementById(jobId).addEventListener('click', () => {
-        alert(`More details about: ${jobData.title}`);
-    });
+  const jobId = `job-${jobData.id}`;
+  document.getElementById(jobId).addEventListener('click', () => {
+    alert(`More details about: ${jobData.title}`);
+  });
 }
