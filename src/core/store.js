857be7d20f3b83650f9cc915e1daba4876b6d23f
lencedev <owen1.bolling@epitech.eu>
2024-10-10 17:29:52 +0200

    remove comments

--- src/core/store.js
+++ src/core/store.js
@@ -12,15 +12,12 @@ export function getState() {
 }
 export function setState(newState) {
     state = { ...state, ...newState };
-    // Notifier tous les écouteurs des changements d'état
     listeners.forEach(listener => listener(state));
 }
 
-// Fonction pour écouter les changements d'état
 export function subscribe(listener) {
     listeners.push(listener);
 
-    // Retourner une fonction pour se désabonner
     return () => {
         const index = listeners.indexOf(listener);
         listeners.splice(index, 1);
